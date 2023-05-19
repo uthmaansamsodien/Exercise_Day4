61f9496d00a98d1fbacaf94d3a16f026edc0f3ec
Uthmaan <samsodienuthmaan@gmail.com>
2023-05-19 09:35:39 +0200

    first push

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let person1= {
+    firstName: "John",
+    Surname: "Cena",
+    Age: 47,
+}
+let person2={
+    firstName: "Dwayne",
+    Surname: "JOhnson",
+    AGe: 51,
+}
+ console.log(person1);
+
+ console.log(person2);
+
+ let person= [person1, person2]
+ console.log(person);
